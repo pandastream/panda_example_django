aad9243ed89a6fdc36c0746ca0eb66146c8d0ba5
Pablo Brasero <pablo@new-bamboo.co.uk>
2010-04-06 15:31:18 +0100

    More work on setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup, find_packages
 
+install_requires = ['panda>=0.1.1']
+try:
+    import json
+except ImportError:
+    install_requires.append('simplejson>=2.1.0')
+
 setup(
     name = "panda_example_django",
-    version = "0.1",
+    version = "0.1.2",
     packages = find_packages(),
     author = "New Bamboo",
     author_email = "info@new-bamboo.co.uk",
     description = "An example of how to integrate Panda on your Django application",
     url = "http://github.com/newbamboo/panda_example_django",
-    install_requires = ['panda>=0.1.1', 'simplejson>=2.1.0'],
+    install_requires=install_requires,
+    include_package_data=True,
 )
